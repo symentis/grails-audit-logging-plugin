e137f966ef1ede83025991ddd3175013d330f197
Shawn Hartsock <hartsock@acm.org>
2012-03-16 14:13:38 -0400

    Revert "A step toward testable events thanks to code from grails-core"
    This reverts commit 5184ebfac20cd53bfe9715cabe958ea064c5e8ca.

--- test/unit/org/codehaus/groovy/grails/plugins/orm/auditable/AuditableRegistryTests.groovy
+++ test/unit/org/codehaus/groovy/grails/plugins/orm/auditable/AuditableRegistryTests.groovy
@@ -1,8 +1,5 @@
 package org.codehaus.groovy.grails.plugins.orm.auditable
 
-import org.codehaus.groovy.grails.commons.GrailsApplication
-import grails.test.GrailsUnitTestCase
-
 /**
  * Created by IntelliJ IDEA.
  * User: hartsock
@@ -10,25 +7,13 @@ import grails.test.GrailsUnitTestCase
  * Time: 8:38 PM
  * To change this template use File | Settings | File Templates.
  */
-class AuditableRegistryTests extends AbstractHibernateTests {
+class AuditableRegistryTests extends GroovyTestCase {
+
    AuditableRegistry auditableRegistry = new AuditableRegistry();
 
-    void loadClasses() {
-        this.gcl.parseClass('''
-// import grails.persistence.*
-
-// @Entity
-class ExampleClassicAuditableEntity {
-    static auditable = true
-    String value
-}
-''')
-    }
-
     void testAuditableRegistryDetection() {
-        def exampleClass = grailsApplication.getDomainClass("ExampleClassicAuditableEntity").clazz
-        assert exampleClass != null
-        assert auditableRegistry.isAuditable(exampleClass) == true
+        def example = ga.getDomainClass("ExampleClassicAuditableEntity")
+        assert example != null
+        assert auditableRegistry.isAuditable(example) == true
     }
 }
-
