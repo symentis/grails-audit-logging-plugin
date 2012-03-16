306e8e95aff29830c295dbc5d733236d8db34c2d
Shawn Hartsock <hartsock@acm.org>
2012-03-16 14:14:39 -0400

    Revert "Revert "A step toward testable events thanks to code from grails-core""
    This reverts commit e137f966ef1ede83025991ddd3175013d330f197.

--- test/unit/org/codehaus/groovy/grails/plugins/orm/auditable/AuditableRegistryTests.groovy
+++ test/unit/org/codehaus/groovy/grails/plugins/orm/auditable/AuditableRegistryTests.groovy
@@ -1,5 +1,8 @@
 package org.codehaus.groovy.grails.plugins.orm.auditable
 
+import org.codehaus.groovy.grails.commons.GrailsApplication
+import grails.test.GrailsUnitTestCase
+
 /**
  * Created by IntelliJ IDEA.
  * User: hartsock
@@ -7,13 +10,25 @@ package org.codehaus.groovy.grails.plugins.orm.auditable
  * Time: 8:38 PM
  * To change this template use File | Settings | File Templates.
  */
-class AuditableRegistryTests extends GroovyTestCase {
-
+class AuditableRegistryTests extends AbstractHibernateTests {
    AuditableRegistry auditableRegistry = new AuditableRegistry();
 
+    void loadClasses() {
+        this.gcl.parseClass('''
+// import grails.persistence.*
+
+// @Entity
+class ExampleClassicAuditableEntity {
+    static auditable = true
+    String value
+}
+''')
+    }
+
     void testAuditableRegistryDetection() {
-        def example = ga.getDomainClass("ExampleClassicAuditableEntity")
-        assert example != null
-        assert auditableRegistry.isAuditable(example) == true
+        def exampleClass = grailsApplication.getDomainClass("ExampleClassicAuditableEntity").clazz
+        assert exampleClass != null
+        assert auditableRegistry.isAuditable(exampleClass) == true
     }
 }
+
---
+++ test/unit/org/codehaus/groovy/grails/plugins/orm/auditable/MockHibernateGrailsPlugin.groovy
@@ -0,0 +1,20 @@
+package org.codehaus.groovy.grails.plugins.orm.auditable
+
+import org.codehaus.groovy.grails.commons.AnnotationDomainClassArtefactHandler
+import org.codehaus.groovy.grails.plugins.orm.hibernate.HibernatePluginSupport
+
+/**
+ *
+ */
+class MockHibernateGrailsPlugin {
+    def version = grails.util.GrailsUtil.getGrailsVersion()
+    def dependsOn = [dataSource: version,
+            i18n: version,
+            core: version,
+            domainClass: version]
+
+    def artefacts = [new AnnotationDomainClassArtefactHandler()]
+    def loadAfter = ['controllers']
+    def doWithSpring = HibernatePluginSupport.doWithSpring
+    def doWithDynamicMethods = HibernatePluginSupport.doWithDynamicMethods
+}
