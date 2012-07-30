093ef67416314f4181e5d5901c4e08c6a14dddd6
Shawn Hartsock <hartsocks@vmware.com>
2012-07-30 08:53:27 -0500

    testing new event listener

---
+++ test/unit/org/codehaus/groovy/grails/plugins/orm/auditable/DomainEventListenerServiceTests.groovy
@@ -0,0 +1,16 @@
+package org.codehaus.groovy.grails.plugins.orm.auditable
+
+
+
+import grails.test.mixin.*
+
+/**
+ * See the API for {@link grails.test.mixin.services.ServiceUnitTestMixin} for usage instructions
+ */
+@TestFor(DomainEventListenerService)
+class DomainEventListenerServiceTests {
+
+    void testSomething() {
+        fail "Implement me"
+    }
+}
