dc3d195c5b4add96543bc4c69690531ba3749af1
Robert Oschwald <roos@symentis.com>
2014-03-11 09:38:07 +0100

    - added helper scripts

---
+++ grails-audit-logging-plugin/doc.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+rm -rf docs/manual
+grails doc --pdf --stacktrace | grep -v javadoc
+#grails doc --stacktrace | grep -v javadoc
+rm -rf docs/manual/api
+rm -rf docs/manual/gapi
+grails add-tracking
