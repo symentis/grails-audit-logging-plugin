737e94740147ca9cd0bc9e0ff031cf1f8eb70cd9
dmahapatro <dhiraj.mahapatro@gmail.com>
2014-05-02 00:51:54 -0400

    Added identifier in the handler map in onSave event.

---
+++ audit-test/grails-app/domain/test/Aircraft.groovy
@@ -0,0 +1,26 @@
+package test
+
+class Aircraft {
+    String type
+    String description
+
+    Map handlersMap = [:]
+    static transients = ['handlersMap']
+
+    static auditable = [handlersOnly : true]
+
+    static mapping = {
+        id name: 'type', generator: 'assigned'
+    }
+
+    static constraints = {
+        type bindable: true
+    }
+
+    def onSave = { newMap ->
+        assert newMap
+        assert !newMap.id
+
+        this.handlersMap = newMap
+    }
+}
