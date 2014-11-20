f544e9b3fd05c51ecd84535cf26868d4ed68aa74
Robert Oschwald <roos@symentis.com>
2014-11-20 18:34:13 +0100

    - set the used audit-test plugins to the recommended versions

--- audit-test/grails-app/conf/BuildConfig.groovy
+++ audit-test/grails-app/conf/BuildConfig.groovy
@@ -48,17 +48,18 @@ grails.project.dependency.resolution = {
 
     plugins {
         // plugins for the build system only
-        build ":tomcat:7.0.50"
+        build ":tomcat:7.0.54"
 
         // plugins for the compile step
-        compile ":scaffolding:2.0.1"
+        compile ":scaffolding:2.0.3"
         compile ':cache:1.1.1'
 
         // plugins needed at runtime but not for compilation
-        runtime ":hibernate:3.6.10.7"
-        // runtime ":hibernate4:4.1.11.7"
+        // Version to use see release notes: https://grails.org/2.3.11+Release+Notes
+        runtime ":hibernate:3.6.10.16"
+        // runtime ":hibernate4:4.3.5.4"
 
-        runtime ":database-migration:1.3.8"
+        runtime ":database-migration:1.4.0"
         runtime ":jquery:1.10.2.2"
         runtime ":resources:1.2.1"
     }
