550eca74d695c08ea636ce6537660fd9a599a12a
Robert Oschwald <robertoschwald@googlemail.com>
2015-06-26 18:04:01 +0200

    - changelog.md, cleanup plugin descriptor

--- audit-logging/src/main/groovy/grails/plugins/orm/auditable/AuditLoggingGrailsPlugin.groovy
+++ audit-logging/src/main/groovy/grails/plugins/orm/auditable/AuditLoggingGrailsPlugin.groovy
@@ -44,46 +44,12 @@ import org.grails.datastore.mapping.core.*
  * See Documentation:
  * http://grails.org/plugin/audit-logging
  *
- * Changes:
- * Release 0.3   actorKey and username features allow for the logging of
- *               user or userPrincipal for most security systems.
- * Release 0.4   custom serializable implementation for AuditLogEvent so events can happen
- *               inside a webflow context.
- *               tweak application.properties for loading in other grails versions
- *               update to views to show URI in an event
- *               fix missing oldState bug in change event
- * Release 0.4.1 repackaged for Grails 1.1.1 see GRAILSPLUGINS-1181
- * Release 0.5_ALPHA see GRAILSPLUGINS-391
- *               changes to AuditLogEvent domain object uses composite id to simplify logging
- *               changes to AuditLogListener uses new domain model with separate transaction
- *               for logging action to avoid invalidating the main hibernate session.
- * Release 0.5_BETA see GRAILSPLUGINS-391
- *               testing version released generally.
- * Release 0.5     GRAILSPLUGINS-391, GRAILSPLUGINS-1496, GRAILSPLUGINS-1181, GRAILSPLUGINS-1515, GRAILSPLUGINS-1811
- * Release 0.5.1   fixes regression in field logging
- * Release 0.5.2   GRAILSPLUGINS-1887 and GRAILSPLUGINS-1354
- * Release 0.5.3   GRAILSPLUGINS-2135 GRAILSPLUGINS-2060 && an issue with extra JAR files that are somehow getting released as part of the plugin
- * Release 0.5.4   compatibility issues with Grails 1.3.x
- * Release 0.5.5   collections logging, log ids, replacement patterns, property value masking, large fields support, fixes and enhancements
- * Release 0.5.5.1 Fixed the title. No changes in the plugin code.
- * Release 0.5.5.2 Added issueManagement to plugin descriptor for the portal. No changes in the plugin code.
- * Release 0.5.5.3 Added ability to disable audit logging by config.
- * Release 1.0.0 Grails >= 2.0 ORM agnostic implementation, major cleanup and new features
- * Release 1.0.1 closures, nonVerboseDelete property, provide domain identifier to onSave() handler
- * Release 1.0.2 GPAUDITLOGGING-66
- * Release 1.0.3 GPAUDITLOGGING-64 workaround for duplicate log entries written per configured dataSource
- *               GPAUDITLOGGING-63 logFullClassName property
- * Release 1.0.4 GPAUDITLOGGING-69 allow to set uri per domain object
- *               GPAUDITLOGGING-62 Add identifier in handler map
- *               GPAUDITLOGGING-29 support configurable id mapping for AuditLogEvent
- *               GPAUDITLOGGING-70 support configurable datasource name for AuditLogEvent
- *               GPAUDITLOGGING-74 Impossible to log values of zero or false
- *               GPAUDITLOGGING-75 Support automatic (audit) stamping support on entities
- *
  */
 
 class AuditLoggingGrailsPlugin extends Plugin {
-    def grailsVersion = '2.0 > *'
+
+    def grailsVersion = '3.0.0 > *'
+
     def title = "Audit Logging Plugin"
     def authorEmail = "roos@symentis.com"
     def description = """ Automatically log change events for domain objects.
@@ -92,16 +58,17 @@ The events include onSave, onChange, and onDelete.
 When called, the event handlers have access to oldObj and newObj definitions that will allow you to take action on what has changed.
     """
 
-    def documentation = 'http://grails.org/plugin/audit-logging'
-    def license = 'APACHE'
-    def organization = [name: "symentis GmbH", url: "http://www.symentis.com/"]
+    String documentation = 'http://grails.org/plugin/audit-logging'
+    String license = 'APACHE'
+    String organization = [name: "symentis GmbH", url: "http://www.symentis.com/"]
     def developers = [
         [ name: 'Robert Oschwald', email: 'roos@symentis.com' ],
         [ name: 'Elmar Kretzer', email: 'elkr@symentis.com' ],
         [ name: 'Aaron Long', email: 'longwa@gmail.com' ]
     ]
-    def issueManagement = [system: 'JIRA', url: 'http://jira.grails.org/browse/GPAUDITLOGGING']
+    def issueManagement = [system: 'github', url: 'https://github.com/robertoschwald/grails-audit-logging-plugin/issues']
     def scm = [url: 'https://github.com/robertoschwald/grails-audit-logging-plugin']
+
     def dependsOn = [:]
     def loadAfter = ['core', 'dataSource']
 
