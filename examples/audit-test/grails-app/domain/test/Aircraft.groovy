00953f4c72c7f8d9c8485b9c692004f18fae43dd
Aaron Long <along@triu.com>
2018-01-30 01:00:20 -0500

    Fix bootRepackage compilation error for examples

--- examples/audit-test/grails-app/domain/test/Aircraft.groovy
+++ examples/audit-test/grails-app/domain/test/Aircraft.groovy
@@ -6,35 +6,11 @@ class Aircraft implements Auditable {
     String type
     String description
 
-    Map handlersMap = [:], handlersOldMap = [:]
-    static transients = ['handlersMap', 'handlersOldMap']
-
     static mapping = {
         id name: 'type', generator: 'assigned'
     }
 
     static constraints = {
         type bindable: true
-    }
-
-    def onSave = { newMap ->
-        assert newMap
-        assert !newMap.id
-
-        this.handlersMap = newMap
-    }
-
-    def onDelete = { oldMap ->
-        assert oldMap
-
-        this.handlersMap = oldMap
-    }
-
-    def onChange = { oldMap, newMap ->
-        assert oldMap && newMap
-        assert !newMap.id && !oldMap.id
-
-        this.handlersOldMap = oldMap
-        this.handlersMap = newMap
     }
 }
--- examples/audit-test/grails-app/domain/test/Airport.groovy
+++ examples/audit-test/grails-app/domain/test/Airport.groovy
@@ -6,32 +6,5 @@ class Airport implements Auditable {
     String code
     String name
 
-    Map handlerMap = [:], handlerOldMap = [:]
-    static transients = ['handlerMap', 'handlerOldMap']
-
     static hasMany = [runways: Runway]
-
-    static auditable = [handlersOnly: true]
-
-    def onSave = { newMap ->
-        assert newMap
-        assert newMap.id instanceof Long
-
-        this.handlerMap = newMap
-    }
-
-    def onDelete = { oldMap ->
-        assert oldMap
-
-        this.handlerMap = oldMap
-    }
-
-    def onChange = { oldMap, newMap ->
-        assert oldMap && oldMap.id
-        assert newMap && oldMap.id
-        assert oldMap.id == newMap.id
-
-        this.handlerMap = newMap
-        this.handlerOldMap = oldMap
-    }
 }
