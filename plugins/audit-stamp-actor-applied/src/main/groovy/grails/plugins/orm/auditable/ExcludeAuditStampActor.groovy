d0902cf8c42a8a12d8fcde5ceed752f36c0013b7
Dennie de Lange <dennie@tkvw.nl>
2018-02-20 23:15:44 +0100

    Removed the applied variants in favour of adding documentation how to implement them

--- plugins/audit-stamp-actor-applied/src/main/groovy/grails/plugins/orm/auditable/ExcludeAuditStampActor.groovy
+++
@@ -1,9 +0,0 @@
-package grails.plugins.orm.auditable
-
-import java.lang.annotation.Retention
-import java.lang.annotation.RetentionPolicy
-
-@Retention(RetentionPolicy.SOURCE)
-@interface ExcludeAuditStampActor {
-
-}
