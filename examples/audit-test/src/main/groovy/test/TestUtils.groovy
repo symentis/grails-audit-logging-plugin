599b88da2630db265b370ce258f071828749ecbe
Robert Oschwald <robertoschwald@googlemail.com>
2019-10-06 11:11:48 +0200

    fixed TedtUtils as PersistentCollections are not logged anymore

--- examples/audit-test/src/main/groovy/test/TestUtils.groovy
+++ examples/audit-test/src/main/groovy/test/TestUtils.groovy
@@ -1,9 +1,17 @@
 package test
 
 import org.grails.datastore.mapping.model.PersistentEntity
+import org.grails.datastore.mapping.model.PersistentProperty
 
 class TestUtils {
     static getAuditableProperties(PersistentEntity entity, List<String> ignoreList) {
-        return entity.persistentProperties.collect { it.name } - ignoreList
+        List<String> properties = entity.persistentProperties.findResults { PersistentProperty p ->
+            if (p.type in Collection){
+                // persistentCollections are not logged anymore. See #153
+                return null
+            }
+            return p.name
+        }
+        return properties - ignoreList
     }
 }
